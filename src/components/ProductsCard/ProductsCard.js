2db37342a5802d69d1bef45006c40d9736be1a94
BerkayTORUN55 <115739149+BerkayTORUN55@users.noreply.github.com>
2025-02-11 23:26:08 +0300

    Add files via upload

---
+++ src/components/ProductsCard/ProductsCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
+import styles from './ProductsCard.style';
+
+const ProductsCard = ({urun}) => {
+    const stokDurumu = urun.inStock ? urun.inStock : 'Stok Yok';
+return (
+    <View style={styles.urun_card}>
+        <TouchableOpacity>
+            <Image style={styles.urun_resim} source={{uri: urun.imgURL}}/>
+            <Text style={styles.urun_baslik}>{urun.title}</Text>
+            <Text style={styles.urun_fiyat}>{urun.price}</Text>
+            <Text style={styles.urun_stok_durumu}>{stokDurumu}</Text>
+        </TouchableOpacity>
+    </View>
+
+    // eslint-disable-next-line semi
+    )};
+
+    export default ProductsCard;
